96e2178c278ca97855bb033f1fdb029c895a65e0
Kotie Smit <kotiesmit@gmail.com>
2017-10-25 20:58:24 +0200

    making ajax calls

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+const API_KEY = "9c03145a38237bc0e4b8bd872902181f";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+
+const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+export default function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},zar`
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
